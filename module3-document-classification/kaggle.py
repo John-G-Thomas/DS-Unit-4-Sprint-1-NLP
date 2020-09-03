b23846493c54adcf70cb579b95174f9da88e93e6
John-G-Thomas <johnthomasg0215@gmail.com>
2020-09-03 18:25:28 -0400

    Assignment Finished

--- module3-document-classification/kaggle.py
+++ module3-document-classification/kaggle.py
@@ -2,7 +2,6 @@ import pandas as pd
 
 files = [
     'data/submission1.csv',
-    'data/submission2.csv',
     'data/submission3.csv',
     'data/submission4.csv',
     'data/submission5.csv',
@@ -19,4 +18,6 @@ test = pd.read_csv("data/test.csv")
 submissions = pd.DataFrame({'id': test['id'], 'ratingCategory': test['id']})
 submissions['ratingCategory'] = majority_vote.astype('int64')
 
-submissions.to_csv('submission-ultra.csv', index=False)
+submissions.to_csv('submission-ultra2.csv', index=False)
+
+
